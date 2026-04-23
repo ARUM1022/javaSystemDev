9910c81416be1afd70a48c4b87efb6ef0e358ea9
saber_terara <saber_terara@172.16.62.198>
2026-04-23 14:57:30 +0900

    terakawa seima

--- src/main/java/scoremanager/LoginAction.java
+++ src/main/java/scoremanager/LoginAction.java
@@ -1,32 +1,39 @@
-package scoremanager;
-
-import bean.School;
-import dao.SchoolDao;
-import dao.TeacherDao;
-import 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import tool.Action;
-
-public class LoginAction extends Action {
-	public void execute(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
-
-		HttpSession session=request.getSession();
-
-		String login=request.getParameter("login");
-		String password=request.getParameter("password");
-
-		School dao=new SchoolDao();
-		School school=dao.search(login, password);
-		
-		if (customer!=null) {
-			session.setAttribute("customer", customer);
-			return "login-out.jsp";
-		}
-		
-		return "error.jsp";
-	}
-}
+//package scoremanager;
+//
+//import bean.School;
+//import dao.SchoolDao;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//import tool.Action;
+//
+//// import bean.School;
+//// import dao.SchoolDao;
+//// import dao.TeacherDao;
+//// import 
+//// import jakarta.servlet.http.HttpServletRequest;
+//// import jakarta.servlet.http.HttpServletResponse;
+//// import jakarta.servlet.http.HttpSession;
+//// import tool.Action;
+//
+//public class LoginAction extends Action {
+//	public void execute(
+//		HttpServletRequest request, HttpServletResponse response
+//	) throws Exception {
+//
+//		HttpSession session=request.getSession();
+//
+//		String login=request.getParameter("login");
+//		String password=request.getParameter("password");
+//
+//		School dao=new SchoolDao();
+//		School school=dao.search(login, password);
+//		
+//		if (customer!=null) {
+//			session.setAttribute("customer", customer);
+//			return "login-out.jsp";
+//		}
+//		
+//		return "error.jsp";
+//	}
+//}
